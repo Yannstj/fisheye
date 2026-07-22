10f0d8d59b7f8dd26a4464ecb6f9d540862d1126
Yannstj <yannick.saintjust@gmail.com>
2026-07-22 18:02:11 +0200

    clean arch

---
+++ dist/controllers/index.controller.js
@@ -0,0 +1,13 @@
+"use strict";
+// import { Photographer, getPhotographers } from "../models/photographer.model.js";
+// import { photographerCardTemplate } from "../views/photographerCard.view.js";
+// function displayData(photographers: Photographer[]): void {
+//   // TODO: reprendre la logique de l'ancien displayData
+//   // (querySelector(".photographer_section"), boucle photographers,
+//   // photographerCardTemplate(photographer).getUserCardDOM(), appendChild)
+// }
+// async function init(): Promise<void> {
+//   // TODO: appelle getPhotographers(), puis displayData(...)
+// }
+// init();
+console.log("cc");
---
+++ dist/models/photographer.model.js
@@ -0,0 +1,5 @@
+export async function getPhotographers() {
+    // TODO: fetch "./data/photographers.json", vérifier response.ok,
+    // parser en JSON (typer le résultat en PhotographersResponse),
+    // puis retourner result.photographers
+}
