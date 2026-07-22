a2a63e39da9882ffe19a2a57f8f455932608bf9c
Yannstj <yannick.saintjust@gmail.com>
2026-07-22 18:15:03 +0200

    oopsi

--- dist/models/photographer.model.js
+++
@@ -1,5 +0,0 @@
-export async function getPhotographers() {
-    // TODO: fetch "./data/photographers.json", vérifier response.ok,
-    // parser en JSON (typer le résultat en PhotographersResponse),
-    // puis retourner result.photographers
-}
--- dist/views/photographerCard.view.js
+++
@@ -1,9 +0,0 @@
-export function photographerCardTemplate(data) {
-    const { name, portrait } = data;
-    const picture = `assets/photographers/${portrait}`;
-    function getUserCardDOM() {
-        // TODO: reprendre la logique de l'ancien scripts/templates/photographer.js
-        // (créer <article>, <img src={picture}>, <h2>{name}</h2>, les assembler, les retourner)
-    }
-    return { name, picture, getUserCardDOM };
-}
