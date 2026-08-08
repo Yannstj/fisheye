f8f49e4dd5aa9249b40672f81c91109b1c86f423
Yannstj <yannick.saintjust@gmail.com>
2026-08-09 00:44:18 +0200

    wip

--- src/views/photographerGallery.ts
+++ src/views/photographerGallery.ts
@@ -3,6 +3,8 @@ import { Media } from "../models/media.js";
 export function photographerGallery(media: Media): DocumentFragment {
   const fragment = document.createDocumentFragment();
 
+  const label = document.createElement("label");
+  const input = document.createElement("input");
   const article = document.createElement("article");
   const mediaImage = document.createElement("img");
   mediaImage.setAttribute("src", media.mediaPath);
