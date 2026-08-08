a7af7bd352d84268520505bdcab013149550fd55
Yannstj <yannick.saintjust@gmail.com>
2026-08-08 15:33:23 +0200

    update sass

--- src/views/photographerBanner.ts
+++ src/views/photographerBanner.ts
@@ -3,18 +3,25 @@ import { createParagraph } from "../utils/dom.js";
 
 export function photographerBanner(photographer: Photographer) {
   const fragment = document.createDocumentFragment();
+  const containerDetails = document.createElement("div");
   const h1 = document.createElement("h1");
   const button = document.createElement("button");
   const img = document.createElement("img");
 
+  containerDetails.setAttribute("class", "photographer_banner_details");
   img.setAttribute("src", photographer.portraitPath);
   img.setAttribute("alt", photographer.name);
-  img.setAttribute("class", "bannerImage");
+  img.setAttribute("class", "photographer_banner_image");
   h1.textContent = photographer.name;
   button.textContent = "Contactez-moi";
-  fragment.appendChild(h1);
-  fragment.appendChild(createParagraph(photographer.fullLocation, "location"));
-  fragment.appendChild(createParagraph(photographer.tagline, "tagline"));
+  containerDetails.appendChild(h1);
+  containerDetails.appendChild(
+    createParagraph(photographer.fullLocation, "location"),
+  );
+  containerDetails.appendChild(
+    createParagraph(photographer.tagline, "tagline"),
+  );
+  fragment.appendChild(containerDetails);
   fragment.appendChild(button);
   fragment.appendChild(img);
   return fragment;
