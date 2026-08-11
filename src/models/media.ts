974a1a5a68f90bf3d9a17567d2f09b60a638fccb
Yannstj <yannick.saintjust@gmail.com>
2026-08-11 21:58:28 +0200

    type union TypeScript

--- src/models/media.ts
+++ src/models/media.ts
@@ -2,35 +2,49 @@ export interface MediaData {
   id: number;
   photographerId: number;
   title: string;
-  image: string;
   likes: number;
   date: string;
   price: number;
 }
 
+interface MediaImageData extends MediaData {
+  image: string;
+}
+interface MediaVideoData extends MediaData {
+  video: string;
+}
+
+type MediaDataType = MediaImageData | MediaVideoData;
+
 export class Media implements MediaData {
   id: number;
   photographerId: number;
   title: string;
-  image: string;
   likes: number;
   date: string;
+  image?: string;
+  video?: string;
   price: number;
   folderName: string;
 
-  constructor(data: MediaData, folderName: string) {
+  constructor(data: MediaDataType, folderName: string) {
     this.id = data.id;
     this.photographerId = data.photographerId;
     this.title = data.title;
-    this.image = data.image;
     this.likes = data.likes;
     this.date = data.date;
     this.price = data.price;
     this.folderName = folderName;
+    if ("image" in data) {
+      this.image = data.image;
+    } else {
+      this.video = data.video;
+    }
   }
 
   get mediaPath() {
-    return `assets/photographers/${this.folderName}/${this.image}`;
+    const filename = this.image ?? this.video;
+    return `assets/photographers/${this.folderName}/${filename}`;
   }
 }
 //TODO asap, gerer le media video
--- src/views/photographerGallery.ts
+++ src/views/photographerGallery.ts
@@ -4,6 +4,8 @@ export function photographerGallery(media: Media): DocumentFragment {
   const fragment = document.createDocumentFragment();
   const article = document.createElement("article");
   const mediaImage = document.createElement("img");
+  const mediaVideo = document.createElement("video");
+  const sourceVideo = document.createElement("source");
   mediaImage.setAttribute("src", media.mediaPath);
   mediaImage.setAttribute("alt", media.title);
   article.appendChild(mediaImage);
